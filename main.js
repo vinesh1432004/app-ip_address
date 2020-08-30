a9e43de9290a53da2291d194f0ee7037e6f6de86
vinesh1432004 <vinesh1432004@gmail.com>
2020-08-30 20:03:33 +0000

    Lab 4

--- main.js
+++ main.js
@@ -47,6 +47,7 @@ class IpAddress {
   // Initialize return arguments for callback
   let firstIpAddress = null;
   let callbackError = null;
+  let IPv6Address = null;
 
   // Instantiate an object from the imported class and assign the instance to variable cidr.
   const cidr = new IPCIDR(cidrStr);
